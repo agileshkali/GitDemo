5d10caf5089208e1f3657ae2b099aff50dd733d5
agi <agiagilesh143@gmail.com>
2022-08-27 09:44:30 +0530

    this is new commit from agi

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -29,5 +29,9 @@ public class TestGreeter {
     String someone = "World";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+    
+ 
   }
 }
